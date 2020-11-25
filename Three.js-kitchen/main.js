8e933972831e304d7d079df337bbed1b3c4173e5
Max Borzenko <zzzking999@yandex.ru>
2020-11-25 16:21:26 +0700

    replaced lamp model with my own lamp

--- Three.js-kitchen/main.js
+++ Three.js-kitchen/main.js
@@ -1,12 +1,7 @@
-const WIDTH = window.innerWidth - 15
-const HEIGHT = window.innerHeight - 100
+const WIDTH = window.innerWidth - 15;
+const HEIGHT = window.innerHeight - 100;
 
-const FOV = 90
-const ASPECT = WIDTH/HEIGHT
-const NEAR = 0.1
-const FAR = 1000
-
-const URL = "https://borzzzenko.github.io/ComputerGraphics/Three.js-kitchen/"
+const URL = "https://borzzzenko.github.io/ComputerGraphics/Three.js-kitchen/";
 
 
 function main() {
@@ -14,6 +9,11 @@ function main() {
 	var scene = new THREE.Scene();
 	scene.background = new THREE.Color(0xAAAAAA);
 
+	const FOV = 90;
+	const ASPECT = WIDTH/HEIGHT;
+	const NEAR = 0.1;
+	const FAR = 1000;
+	
 	var camera = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
 	camera.position.z = 5;
 
@@ -23,12 +23,13 @@ function main() {
 	
 	// Floor
 	const planeSize = 10;
-
 	const loader = new THREE.TextureLoader();
+	
 	const texture = loader.load(URL + "textures/wood-floor.jpg");
 	texture.wrapS = THREE.RepeatWrapping;
 	texture.wrapT = THREE.RepeatWrapping;
 	texture.magFilter = THREE.NearestFilter;
+	
 	const repeats = planeSize / 2;
 	texture.repeat.set(repeats, repeats);
 
@@ -37,45 +38,72 @@ function main() {
 		map: texture,
 		side: THREE.DoubleSide,
 	});
+	
 	const floor = new THREE.Mesh(planeGeo, planeMat);
-	floor.rotation.x = Math.PI * -.5;
-	floor.position.y += 0.1
+	floor.rotation.x = Math.PI * -0.5;
+	floor.position.y += 0.1;
+	
 	scene.add(floor);
 
 	// Adding room walls
-	var geometry = new THREE.BoxGeometry(10, 5, 10);
+	let width = 10;
+	let height = 5;
+	let depth = 10;
+	
+	var geometry = new THREE.BoxGeometry(width, height, depth);
 	var material = new THREE.MeshPhongMaterial({
 		color: 0xffffff,
 		side: THREE.DoubleSide
 	});
-	var roomCube = new THREE.Mesh(geometry, material);
-	roomCube.position.y += 2.5
-	scene.add(roomCube);
 
-	// Light
-	const light = new THREE.PointLight( 0xffffff, 1, 15);
-	light.position.set(0, 4, 0);
-	scene.add( light );
+	var roomCube = new THREE.Mesh(geometry, material);
+	roomCube.position.y += 2.5;
+	
+	scene.add(roomCube);
 
 	// Camera controls
 	const controls = new THREE.OrbitControls(camera, renderer.domElement);
 	controls.update();
 
-	// Load Lamps model
-	const mtlLoader = new THREE.MTLLoader();
-	mtlLoader.setPath(URL + "models/")
-	mtlLoader.load("lamp.mtl", (materials) => {
-		materials.preload();
+	// Lamp fiber
+	var fiberRadius = 0.2;
+	var fiberHeight = 5;
+	
+	var fiberGeometry = new THREE.CylinderGeometry(fiberRadius,
+		 fiberRadius, fiberHeight);
+	var fiberMaterial = new THREE.MeshPhongMaterial();
+	
+	var lampFiber = new THREE.Mesh(fiberGeometry, fiberMaterial);
+	lampFiber.position.y += 4.7;
+	lampFiber.scale.set(0.1, 0.1, 0.1);
+	
+	scene.add(lampFiber);
+	
+	// Lamp
+	var lampRadius = 2
+	var lampSegments = 64
 
-		const objLoader = new THREE.OBJLoader();
-		objLoader.setMaterials(materials);
-		objLoader.setPath(URL + "models/");
-		objLoader.load("lamp.obj", (root) => {
-				root.scale.set(0.0025, 0.0025, 0.0025)
-				root.position.y += 4.33
-				scene.add(root);
-		});
-	})
+	var lampGeometry = new THREE.SphereGeometry(lampRadius, lampSegments,
+		 lampSegments);
+	var lampMaterial = new THREE.MeshBasicMaterial({
+		color: 0xffffff,
+	});
+	
+	var lamp = new THREE.Mesh(lampGeometry, lampMaterial);
+	lamp.position.y += 4.5;
+	lamp.scale.set(0.1, 0.1, 0.1);
+	
+	scene.add(lamp);
+
+	// Lamp light
+	var lampLightIntensity = 1;
+	var lampLightDistance = 15;
+
+	const lampLight = new THREE.PointLight( 0xffffff, lampLightIntensity,
+		 lampLightDistance);
+	lampLight.position.set(0, 4, 0);
+	
+	scene.add(lampLight);
 
 	// Animation loop
 	var animate = function() {
@@ -90,5 +118,3 @@ function main() {
 }
 
 main();
-
-
